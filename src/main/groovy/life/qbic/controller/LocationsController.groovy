6cd76c7eba30719507519c1cbdecd51c24540f97
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-03-23 14:48:42 +0100

    Provides additional openAPI specs

--- src/main/groovy/life/qbic/controller/LocationsController.groovy
+++ src/main/groovy/life/qbic/controller/LocationsController.groovy
@@ -8,6 +8,12 @@ import io.micronaut.context.annotation.Parameter
 import io.micronaut.http.HttpResponse
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import life.qbic.datamodel.services.Sample
 import life.qbic.micronaututils.auth.Authentication
 
 import javax.annotation.security.RolesAllowed
@@ -29,6 +35,16 @@ class LocationsController {
   }
 
   @Get(uri = "/contacts/{email}", produces = MediaType.APPLICATION_JSON)
+  @Operation(summary = "Provides the contact information linked to an e-mail",
+          description = "Provides detailed contact information that is linked to an e-mail",
+          tags = "Contact")
+  @ApiResponse(responseCode = "200", description = "Current contact associated with the email address",
+                content = @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = Contact.class)))
+  @ApiResponse(responseCode = "400", description = "Invalid e-mail address")
+  @ApiResponse(responseCode = "401", description = "Unauthorized access")
+  @ApiResponse(responseCode = "404", description = "Contact not found")
   @RolesAllowed(["READER", "WRITER"])
   HttpResponse<Contact> contacts(@Parameter('email') String email){
     if(!RegExValidator.isValidMail(email)) {
@@ -48,6 +64,16 @@ class LocationsController {
   }
 
   @Get(uri = "/{contact_email}", produces = MediaType.APPLICATION_JSON)
+  @Operation(summary = "Provides the locations information linked to an e-mail",
+          description = "Provides detailed locations information that is linked to an e-mail",
+          tags = "Contact")
+  @ApiResponse(responseCode = "200", description = "Current locations associated with the email address",
+          content = @Content(
+                  mediaType = "application/json",
+                  array = @ArraySchema(schema = @Schema(implementation = Location.class))))
+  @ApiResponse(responseCode = "400", description = "Invalid e-mail address")
+  @ApiResponse(responseCode = "401", description = "Unauthorized access")
+  @ApiResponse(responseCode = "404", description = "Location not found")
   @RolesAllowed(["READER", "WRITER"])
   HttpResponse<List<Location>> locations(@Parameter('contact_email') String contact_email){
     if(!RegExValidator.isValidMail(contact_email)) {
@@ -60,6 +86,14 @@ class LocationsController {
   }
 
   @Get(uri = "/", produces = MediaType.APPLICATION_JSON)
+  @Operation(summary = "Provides all available locations",
+          description = "Provides all available locations",
+          tags = "Location")
+  @ApiResponse(responseCode = "200", description = "All available locations",
+          content = @Content(
+                  mediaType = "application/json",
+                  array = @ArraySchema(schema = @Schema(implementation = Location.class))))
+  @ApiResponse(responseCode = "401", description = "Unauthorized access")
   @RolesAllowed(["READER", "WRITER"])
   HttpResponse<List<Location>> listLocations() {
     List<Location> res = locService.listLocations()
