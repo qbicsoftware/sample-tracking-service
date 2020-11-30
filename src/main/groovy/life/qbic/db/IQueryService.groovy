1ef2345a8469f38a3397b3d56ce85f548ab63b45
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2020-11-30 10:40:50 +0100

    Add TODOs for missing interface documentation

--- src/main/groovy/life/qbic/db/IQueryService.groovy
+++ src/main/groovy/life/qbic/db/IQueryService.groovy
@@ -4,20 +4,67 @@ import javax.inject.Singleton
 import io.micronaut.http.HttpResponse
 import life.qbic.datamodel.services.*
 
+/**
+ * //TODO describe this interface
+ */
 @Singleton
 interface IQueryService {
 
+  /**
+   * //TODO describe intended method behaviour
+   * @param email
+   * @return TODO
+   * @since TODO
+   */
   Contact searchPersonByEmail(String email)
-  
+
+  /**
+   * //TODO describe intended method behaviour
+   * @return TODO
+   * @since TODO
+   */
   List<Location> listLocations()
-  
+
+  /**
+   * //TODO describe intended method behaviour
+   * @param email
+   * @return TODO
+   * @since TODO
+   */
   List<Location> getLocationsForEmail(String email)
 
+  /**
+   * //TODO describe intended method behaviour
+   * @param sampleId
+   * @param location
+   * @return TODO
+   * @since TODO
+   */
   HttpResponse addNewLocation(String sampleId, Location location)
 
+  /**
+   * //TODO describe intended method behaviour
+   * @param sampleId
+   * @param location
+   * @return TODO
+   * @since TODO
+   */
   HttpResponse updateLocation(String sampleId, Location location)
 
+  /**
+   * //TODO describe intended method behaviour
+   * @param sampleId
+   * @return TODO
+   * @since TODO
+   */
   Sample searchSample(String sampleId)
 
+  /**
+   * //TODO describe intended method behaviour
+   * @param sampleId
+   * @param status
+   * @return TODO
+   * @since TODO
+   */
   boolean updateSampleStatus(String sampleId, Status status)
 }
--- src/main/groovy/life/qbic/service/ILocationService.groovy
+++ src/main/groovy/life/qbic/service/ILocationService.groovy
@@ -4,12 +4,32 @@ import javax.inject.Singleton
 import io.micronaut.http.HttpResponse
 import life.qbic.datamodel.services.*
 
+/**
+ * //TODO describe this interface
+ */
 @Singleton
 interface ILocationService {
 
+  /**
+   * //TODO describe intended method behaviour
+   * @param email
+   * @return TODO
+   * @since TODO
+   */
   Contact searchPersonByEmail(String email)
-  
+
+  /**
+   * //TODO describe intended method behaviour
+   * @return TODO
+   * @since TODO
+   */
   List<Location> listLocations()
-  
+
+  /**
+   * //TODO describe intended method behaviour
+   * @param email
+   * @return TODO
+   * @since TODO
+   */
   List<Location> getLocationsForEmail(String email)
 }
