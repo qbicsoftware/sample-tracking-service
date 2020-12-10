a1fa5be2287fb5ff23d066c0b9be432c6ac0f121
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2020-12-10 10:22:44 +0100

    Change string body to status reason

--- src/main/groovy/life/qbic/controller/SamplesController.groovy
+++ src/main/groovy/life/qbic/controller/SamplesController.groovy
@@ -1,26 +1,25 @@
 package life.qbic.controller
 
-import io.micronaut.context.annotation.Parameter
+
 import io.micronaut.context.annotation.Requires
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.MediaType
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.PathVariable
-import io.micronaut.http.annotation.Post
-import io.micronaut.http.annotation.Put
+import io.micronaut.http.annotation.*
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import life.qbic.datamodel.services.Location
+import life.qbic.datamodel.services.Sample
+import life.qbic.datamodel.services.Status
 import life.qbic.micronaututils.auth.Authentication
+import life.qbic.service.ISampleService
 
 import javax.annotation.security.RolesAllowed
 import javax.inject.Inject
-import life.qbic.datamodel.services.*
-import life.qbic.service.ISampleService
 
 @Requires(beans = Authentication.class)
 @Secured(SecurityRule.IS_AUTHENTICATED)
@@ -48,13 +47,13 @@ class SamplesController {
   @RolesAllowed([ "READER", "WRITER"])
   HttpResponse<Sample> sample(@PathVariable('sampleId') String code) {
     if(!RegExValidator.isValidSampleCode(code)) {
-      return HttpResponse.badRequest("Not a valid sample code!");
+      return HttpResponse.status(HttpStatus.BAD_REQUEST, "Not a valid sample code!")
     } else {
       Sample s = sampleService.searchSample(code);
       if(s!=null) {
         return HttpResponse.ok(s);
       } else {
-        return HttpResponse.notFound("Sample was not found in the system!");
+        return HttpResponse.status(HttpStatus.NOT_FOUND, "Sample was not found in the system!")
       }
     }
   }
@@ -70,7 +69,7 @@ class SamplesController {
   @RolesAllowed("WRITER")
   HttpResponse<Location> newLocation(@PathVariable('sampleId') String sampleId, Location location) {
     if(!RegExValidator.isValidSampleCode(sampleId)) {
-      return HttpResponse.badRequest("Not a valid sample code!");
+      return HttpResponse.status(HttpStatus.BAD_REQUEST, "Not a valid sample code!")
     } else {
       return sampleService.addNewLocation(sampleId, location)
     }
@@ -93,7 +92,7 @@ class SamplesController {
   @RolesAllowed("WRITER")
   HttpResponse<Location> updateLocation(@PathVariable('sampleId') String sampleId, Location location) {
     if(!RegExValidator.isValidSampleCode(sampleId)) {
-      return HttpResponse.badRequest("Not a valid sample code!");
+      return HttpResponse.status(HttpStatus.BAD_REQUEST, "Not a valid sample code!")
     } else {
       return sampleService.updateLocation(sampleId, location)
     }
@@ -110,14 +109,14 @@ class SamplesController {
   @RolesAllowed("WRITER")
   HttpResponse sampleStatus(@PathVariable('sampleId') String sampleId, @PathVariable('status') Status status) {
     if(!RegExValidator.isValidSampleCode(sampleId)) {
-      return HttpResponse.badRequest("Not a valid sample code!");
+      return HttpResponse.status(HttpStatus.BAD_REQUEST, "Not a valid sample code!")
     }
     boolean found = sampleService.searchSample(sampleId)!=null;
     if(found) {
       sampleService.updateSampleStatus(sampleId, status);
-      return HttpResponse.created("Sample status updated.");
+      return HttpResponse.status(HttpStatus.CREATED, "Sample status updated.")
     } else {
-      return HttpResponse.notFound("Sample was not found in the system!");
+      return HttpResponse.status(HttpStatus.NOT_FOUND, "Sample was not found in the system!")
     }
   }
 }
