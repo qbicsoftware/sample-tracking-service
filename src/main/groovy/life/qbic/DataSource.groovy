84ce2fe2b00f074f934ec5bcaab432d6eb4a1c6e
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-11-23 08:50:49 +0100

    Provide JavaDoc for DataSource interface

--- src/main/groovy/life/qbic/DataSource.groovy
+++ src/main/groovy/life/qbic/DataSource.groovy
@@ -2,8 +2,24 @@ package life.qbic
 
 import java.sql.Connection
 
+/**
+ * <b>Interface Datasource</b>
+ *
+ * <p>A source that provides connections to a persistent datasource.</p>
+ * @since 1.2.1
+ */
 interface DataSource {
 
+    /**
+     * Provides a new {@link Connection} object instance to be able to to submit
+     * queries to the underlying datasource.
+     *
+     * The caller MUST handle the closing of the connection, after the connection is not used anymore.
+     *
+     * Since the {@link Connection} class implements the {@link AutoCloseable} interface, you wrap the
+     * execution of the connection object with a try-with-resource statement or call the
+     * {@link AutoCloseable#close()} method explicitly.
+     * @return a connection to the datasource
+     */
     Connection getConnection()
-
 }
