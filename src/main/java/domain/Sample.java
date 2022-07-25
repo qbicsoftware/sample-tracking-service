014519691d5ca7c86fd76e26cfe69257039c701b
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-07-25 09:32:10 +0200

    Move decision on when and how to move to the Sample.java

--- src/main/java/domain/Sample.java
+++ src/main/java/domain/Sample.java
@@ -1,6 +1,16 @@
 package domain;
 
+import static domain.Status.DATA_AVAILABLE;
+import static domain.Status.LIBRARY_PREP_FINISHED;
+import static domain.Status.METADATA_REGISTERED;
+import static domain.Status.SAMPLE_QC_FAILED;
+import static domain.Status.SAMPLE_QC_PASSED;
+import static domain.Status.SAMPLE_RECEIVED;
+import static domain.Status.SEQUENCING_COMPLETED;
+
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>A sample in the context of sample-tracking.</p>
@@ -12,6 +22,8 @@ public class Sample {
   private final SampleCode sampleCode;
   private Status status;
 
+  private Instant latestProcessedEvent = Instant.MIN;
+
   private final SampleEventPublisher eventPublisher;
 
   public Sample(SampleCode sampleCode, SampleEventPublisher eventPublisher) {
@@ -19,7 +31,17 @@ public class Sample {
     this.eventPublisher = eventPublisher;
   }
 
+
+  public void handleEvents(SampleEvent... sampleEvents) {
+    for (SampleEvent sampleEvent : sampleEvents) {
+      handleEvent(sampleEvent);
+    }
+  }
+
   public void handleEvent(SampleEvent sampleEvent) {
+    if (sampleEvent.occurredOn().isBefore(latestProcessedEvent)) {
+      return; // do only handle events newer than the latest
+    }
     if (sampleEvent instanceof SampleStatusChanged) {
       handleSampleStatusChanged((SampleStatusChanged) sampleEvent);
     }
@@ -27,22 +49,26 @@ public class Sample {
 
   private void handleSampleStatusChanged(SampleStatusChanged sampleStatusChanged) {
     Status status = sampleStatusChanged.status();
-    if (!status.equals(this.status)) {
-      if (status.canFollow(this.status)) {
-        this.status = status;
-      }
+    if (status.equals(this.status)) {
+      return; // no move necessary
     }
+    if (canMoveToStatus(status)) {
+      this.status = status;
+    }
+    this.latestProcessedEvent = sampleStatusChanged.occurredOn();
   }
 
 
   /**
    * Moves the sample status to the provided status at the provided time.
-   * @param status the status to move to
+   *
+   * @param status    the status to move to
    * @param changedAt the instant the status went into effect.
    * @throws IllegalArgumentException in case the sample cannot move.
    */
   public void moveToStatus(Status status, Instant changedAt) {
-    if (!status.canFollow(this.status)) {
+    //assume the status entry to be the newest one
+    if (!canMoveToStatus(status)) {
       throw new IllegalArgumentException(
           String.format("Status %s cannot follow on %s", status, this.status));
     }
@@ -51,4 +77,37 @@ public class Sample {
         changedAt);
     eventPublisher.publish(sampleStatusChanged);
   }
+
+
+  /**
+   * Holding a status as key with all its direct predecessors.
+   */
+  private static final Map<Status, Status> STATUS_AND_PREDECESSORS = initPredecessorMap();
+
+  /**
+   * Determines whether the sample can move to a specific status based on the samples current
+   * status.
+   *
+   * @param status the status to move to
+   * @return true if the move is valid; false otherwise
+   */
+  private boolean canMoveToStatus(Status status) {
+    return STATUS_AND_PREDECESSORS.get(status).equals(this.status);
+  }
+
+  /**
+   * @return a map containing a Status as key and direct predecessors as value.
+   */
+  private static Map<Status, Status> initPredecessorMap() {
+    Map<Status, Status> predecessor = new HashMap<>();
+    predecessor.put(SAMPLE_QC_PASSED, SAMPLE_RECEIVED);
+    predecessor.put(SAMPLE_QC_FAILED, SAMPLE_RECEIVED);
+    predecessor.put(LIBRARY_PREP_FINISHED, SAMPLE_QC_PASSED);
+    predecessor.put(SEQUENCING_COMPLETED, LIBRARY_PREP_FINISHED);
+    predecessor.put(METADATA_REGISTERED, SEQUENCING_COMPLETED);
+    predecessor.put(DATA_AVAILABLE, METADATA_REGISTERED);
+
+    return predecessor;
+  }
+
 }
--- src/main/java/domain/Status.java
+++ src/main/java/domain/Status.java
@@ -1,8 +1,5 @@
 package domain;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * <p>A status a sample can have. It denotes a distinct state of the sample life-cycle.</p>
  */
@@ -13,34 +10,5 @@ public enum Status {
   LIBRARY_PREP_FINISHED,
   SEQUENCING_COMPLETED,
   METADATA_REGISTERED,
-  DATA_AVAILABLE;
-
-  /**
-   * Holding a status as key with all its direct precursors.
-   */
-  private static final Map<Status, Status> STATUS_AND_PRECURSOR = initPrecursorMap();
-
-  /**
-   * @return a map containing a Status as key and direct precursors as value.
-   */
-  private static Map<Status, Status> initPrecursorMap() {
-    Map<Status, Status> precursor = new HashMap<>();
-    precursor.put(SAMPLE_QC_PASSED, SAMPLE_RECEIVED);
-    precursor.put(SAMPLE_QC_FAILED, SAMPLE_RECEIVED);
-    precursor.put(LIBRARY_PREP_FINISHED, SAMPLE_QC_PASSED);
-    precursor.put(SEQUENCING_COMPLETED, LIBRARY_PREP_FINISHED);
-    precursor.put(METADATA_REGISTERED, SEQUENCING_COMPLETED);
-    precursor.put(DATA_AVAILABLE, METADATA_REGISTERED);
-
-    return precursor;
-  }
-
-  /**
-   * This method determines whether this sample status can follow on a provided sample status.
-   * @param previous the sample status the sample was in previously.
-   * @return whether this sample status is a successor of the provided status.
-   */
-  public boolean canFollow(Status previous) {
-    return STATUS_AND_PRECURSOR.get(this).equals(previous);
-  }
+  DATA_AVAILABLE
 }
