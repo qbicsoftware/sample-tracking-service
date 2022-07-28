5c6095ff38effb88146d7b4e0d87016e71640247
Tobias Koch <KochTobi@users.noreply.github.com>
2022-07-28 16:16:02 +0200

    Fix typo
    Co-authored-by: wow-such-code <andreas.friedrich@uni-tuebingen.de>

--- src/main/groovy/life/qbic/controller/RegExValidator.groovy
+++ src/main/groovy/life/qbic/controller/RegExValidator.groovy
@@ -11,7 +11,7 @@ class RegExValidator {
   private static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile('^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$', Pattern.CASE_INSENSITIVE);
   private static final Pattern VALID_QBIC_SAMPLE_CODE = Pattern.compile('(Q[A-X0-9]{4}[0-9]{3}[A-X][A-X0-9]$)|(Q[A-X0-9]{4}ENTITY-[0-9]*$)', Pattern.CASE_INSENSITIVE);
 
-  public static boolean sValidMail(String mail) {
+  public static boolean isValidMail(String mail) {
     Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(mail);
     boolean res = matcher.find()
     return res
