860d9e628baa2ad8f0889ce816fad7fab47df627
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-08-10 18:09:41 +0200

    Implement test fpr deserialization of sample events

--- src/test/groovy/life/qbic/infrastructure/serialization/event/EventDeserializerFactorySpec.groovy
+++ src/test/groovy/life/qbic/infrastructure/serialization/event/EventDeserializerFactorySpec.groovy
@@ -1,24 +1,32 @@
 package life.qbic.infrastructure.serialization.event
 
 
+import life.qbic.domain.sample.events.*
 import spock.lang.Specification
 
-/**
- * <b>short description</b>
- *
- * <p>detailed description</p>
- *
- * @since <version tag>
- */
 class EventDeserializerFactorySpec extends Specification {
 
-  def "test"() {
+  def "deserializer obtained by factory works for #className"() {
     given:
     def deserializer = EventDeserializerFactory.sampleEventDeserializer()
-    def json = '{"className":"life.qbic.domain.sample.events.FailedQualityControl","version":"1.0","sampleCode":"QABCD001A0","occurredOn":"2022-08-10T06:34:40.147Z"}'
+    def json = '{"className":"' + className + '","version":"1.0","sampleCode":"QABCD001A0","occurredOn":"2022-08-10T06:34:40.147Z"}'
 
-    expect:
-    println deserializer.deserialize(json)
+    when:
+    def event = deserializer.deserialize(json)
+    then:
+    Objects.nonNull(event)
+    eventClass.isInstance(event)
+
+    where:
+    eventClass << [
+            MetadataRegistered.class,
+            SampleReceived.class,
+            FailedQualityControl.class,
+            PassedQualityControl.class,
+            LibraryPrepared.class,
+            DataMadeAvailable.class
+    ]
+    className = eventClass.name
 
   }
 }
