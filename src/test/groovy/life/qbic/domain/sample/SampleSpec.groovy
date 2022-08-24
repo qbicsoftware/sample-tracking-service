526f2d98e76315d2f65c3e0c3f3996e2498e9a71
Tobias Koch <KochTobi@users.noreply.github.com>
2022-08-24 11:22:03 +0200

    Allow sample history modification (#106)
    * Allow sample history modification
    
    * Adjust tests
    
    * Adjust tests
    
    * test for rejection of DIFFERENT event

--- src/test/groovy/life/qbic/domain/sample/SampleSpec.groovy
+++ src/test/groovy/life/qbic/domain/sample/SampleSpec.groovy
@@ -23,7 +23,7 @@ class SampleSpec extends Specification {
     sample.sampleCode() == sampleCode
     where:
     expectedStatus             | metadataRegisteredOccurredOn              | sampleReceivedOccurredOn                  | dataAvailableOccurredOn
-    Status.METADATA_REGISTERED | Instant.MAX | Instant.parse("2022-07-27T00:00:01.000Z") | Instant.MIN
+    Status.METADATA_REGISTERED | Instant.MAX                               | Instant.parse("2022-07-27T00:00:01.000Z") | Instant.MIN
     Status.SAMPLE_RECEIVED     | Instant.parse("2022-07-27T00:00:01.000Z") | Instant.MAX                               | Instant.MIN
     Status.DATA_AVAILABLE      | Instant.MIN                               | Instant.parse("2022-07-27T00:00:01.000Z") | Instant.MAX
   }
@@ -43,19 +43,42 @@ class SampleSpec extends Specification {
     stateAfter == stateBefore
   }
 
-  def "given a sample and an event predating the sample state, when the event is added to the sample, then an Exception is thrown"() {
+  def "given a sample and an event predating the sample state, when the event is added to the sample, then the event is integrated in the sample history"() {
     given: "a Sample with at least one event"
     SampleCode sampleCode = SampleCode.fromString("QABCD001A0")
-    MetadataRegistered metadataRegistered = MetadataRegistered.create(sampleCode, Instant.MAX)
-    Sample sample = Sample.fromEvents([metadataRegistered])
+    MetadataRegistered metadataRegistered = MetadataRegistered.create(sampleCode, Instant.MIN)
+    DataMadeAvailable dataMadeAvailable = DataMadeAvailable.create(sampleCode, Instant.MAX)
+    Sample sample = Sample.fromEvents([metadataRegistered, dataMadeAvailable])
     and: "an event predating the current state of the sample"
-    SampleReceived sampleReceived = SampleReceived.create(sampleCode, Instant.MIN)
+    SampleReceived sampleReceived = SampleReceived.create(sampleCode, Instant.parse("2000-01-01T00:00:01.000Z"))
     when: "the event is added to the sample"
     sample.handle(sampleReceived)
-    then: "the event was not added to the sample"
-    !sample.events().contains(sampleReceived)
-    and: "an exception is thrown"
+    then: "the event was is added in the middle"
+    sample.events().get(0) == metadataRegistered
+    sample.events().get(1) == sampleReceived
+    sample.events().get(2) == dataMadeAvailable
+    and:
+    sample.currentState().status() == Status.DATA_AVAILABLE
+  }
+
+  def "given a sample that has seen an event and an event at the same time: when the event is added to the sample, then an UnrecoverableException is thrown"() {
+    given: "a sample that has seen an event and an event at the same time"
+    def metadataRegistered = MetadataRegistered.create(SampleCode.fromString("QABCD001A0"), Instant.MIN)
+    def sampleReceived = SampleReceived.create(SampleCode.fromString("QABCD001A0"), Instant.parse("2000-01-01T00:00:01.000Z"))
+    def dataMadeAvailable = DataMadeAvailable.create(SampleCode.fromString("QABCD001A0"), Instant.MAX)
+
+    Sample sample = Sample.fromEvents([metadataRegistered, sampleReceived, dataMadeAvailable])
+
+    when: "the event is added to the sample"
+    sample.handle(event)
+
+    then: "an UnrecoverableException is thrown"
     thrown(UnrecoverableException)
+
+    where:
+    event << [PassedQualityControl.create(SampleCode.fromString("QABCD001A0"), Instant.MIN),
+              FailedQualityControl.create(SampleCode.fromString("QABCD001A0"), Instant.parse("2000-01-01T00:00:01.000Z")),
+              MetadataRegistered.create(SampleCode.fromString("QABCD001A0"), Instant.MAX)]
   }
 
   def "expect sample creation from events not possible if no events are provided"() {
